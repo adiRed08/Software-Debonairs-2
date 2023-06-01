a44e75d87fa2557380a9fd3906a0c2d5963d7223
adiRed08 <119784941+adiRed08@users.noreply.github.com>
2023-06-01 22:49:01 +0800

    hopefully nothing will go wrong
    redid the implementation of the battling because github merged it improperly

--- Assets/HealthBarMechanics.cs
+++ Assets/HealthBarMechanics.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class HealthBarMechanics : MonoBehaviour
 {
     public Slider health;
+    public TMP_Text valueText;
     // Start is called before the first frame update
     void Start()
     {
         health.value = 100;
+        health.maxValue = 100;
+        valueText.text = health.value.ToString() + "/" + health.maxValue.ToString(); 
     }
     
     public void deduct_health()
