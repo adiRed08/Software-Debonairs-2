7d7249fd5738e026420f0f1bb8ef3ebeb8069442
adiRed08 <119784941+adiRed08@users.noreply.github.com>
2023-06-01 21:32:09 +0800

    Merge pull request #13 from adiRed08/revert-12-fully-working-battle-without-items
    Revert "Fully working battle without items"

--- Assets/Opponent.cs
+++ Assets/Opponent.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
 [CreateAssetMenu]
@@ -10,5 +9,4 @@ public class Opponent : ScriptableObject
     public Sprite image;
     public string name;
     public string[] statements;
-    public string endRemark;
 }
